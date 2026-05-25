b9eb9be439c589e1baa66456c67db6d337fffc0b
Radislav <vladimir_3214241@mail.ru>
2026-05-25 14:52:21 +0300

    Move sidebar data to context processor

---
+++ questions/context_processors.py
@@ -0,0 +1,18 @@
+def sidebar_data(request):
+    return {
+        "popular_tags": [
+            "мнения",
+            "другое",
+            "игры",
+            "учёба",
+            "школа",
+            "общение",
+        ],
+        "top_users": [
+            "ggadk_batman",
+            "wenIafe",
+            "perry_ytkonos_zov4",
+            "cedar_5545",
+            "Биба и Боба",
+        ],
+    }

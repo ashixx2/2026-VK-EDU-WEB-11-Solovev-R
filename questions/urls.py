3011e20b80ee80e7b02d48202897c8ed03b282a7
Radislav <vladimir_3214241@mail.ru>
2026-05-26 13:00:48 +0300

    Add AJAX voting and correct answer selection

--- questions/urls.py
+++ questions/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'questions'
+
+app_name = "questions"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('hot/', views.hot, name='hot'),
-    path('tag/<str:tag_name>/', views.tag, name='tag'),
-    path('question/<int:question_id>/', views.question_detail, name='question_detail'),
-    path('ask/', views.ask, name='ask'),
+    path("", views.index, name="index"),
+    path("hot/", views.hot, name="hot"),
+    path("tag/<str:tag_name>/", views.tag, name="tag"),
+    path("question/<int:question_id>/", views.question_detail, name="question_detail"),
+    path("ask/", views.ask, name="ask"),
+
+    path("ajax/vote-question/", views.vote_question, name="vote_question"),
+    path("ajax/vote-answer/", views.vote_answer, name="vote_answer"),
+    path("ajax/mark-correct-answer/", views.mark_correct_answer, name="mark_correct_answer"),
 ]
